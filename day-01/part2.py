42d245f6ced0f4cd304177d4010b9cdada996e78
Silas Berger <silas.berger@outlook.com>
2025-12-11 21:03:33 +0100

    Solve D1P2.

---
+++ day-01/part2.py
@@ -0,0 +1,36 @@
+def calc_position(current_position, direction, value):
+    if direction == 'L':
+        new_position = (current_position - value) % 100
+        zeros = abs((current_position - value) // 100)
+        if current_position == 0:  # (0-50)//100 = -1, but we've already counted that 0 when we got to it.
+            zeros -= 1
+        if new_position == 0:  # (50-50)//100 = 0, but we need to count this one
+            zeros += 1
+        return new_position, zeros
+    elif direction == 'R':
+        new_position = (current_position + value) % 100
+        zeros = (current_position + value) // 100
+        return new_position, zeros
+    else:
+        raise Exception(f'Unexpected direction: {direction}')
+    
+
+with open("input.txt") as f:
+    lines = [[line[0], int(line[1:-1])] for line in f.readlines()]
+
+position = 50
+zero_count = 0
+for line in lines:
+    new_position, new_zeros = calc_position(position, line[0], line[1])
+    print(f'{position}, {line} -> {new_position} ({new_zeros})')
+    position = new_position
+    zero_count += new_zeros
+
+print(f'Zero count: {zero_count}')
+    
+# 26, ['L', 26] -> 0 (0)
+# 0, ['L', 98] -> 2 (1)
+# 2, ['L', 17] -> 85 (1)
+
+# 13, ['R', 487] -> 0 (5)
+# 0, ['L', 39] -> 61 (1)
